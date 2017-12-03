c8f53414371708c8244b6c52f973d6994d95f128
LAV0Y <david@monteltech.com>
2017-12-03 17:24:02 -0600

    added pawn api

--- routes/pawn.js
+++ routes/pawn.js
@@ -1,25 +1,70 @@
-'use strict'; 
+"use strict";
 
-var mysql = require('mysql');
-var request = require('request');
+var mysql = require("mysql");
+var request = require("request");
 
 var connection = mysql.createPool({
-    multipleStatements: true,
-    host: '192.168.202.159',
-    user: 'admin',
-    password: 'admin',
-    database: 'ogc'
+	multipleStatements: true,
+	host: "192.168.202.159",
+	user: "admin",
+	password: "admin",
+	database: "ogc"
 });
-const uuidV4 = require('uuid/v4');
+const uuidV4 = require("uuid/v4");
 
-exports.homepage = function(req,res){ 
-	connection.getConnection(function(err, connection){
+exports.homepage = function(req, res) {
+	connection.getConnection(function(err, connection) {
 		if (err) throw err;
-		var query = connection.query('SELECT * FROM pawn',  function(err,rows){
-			if (err)
-				console.log("Error Selecting : %s", err);
-			res.render('pages/pawn', {title:'All Parts', rows:rows});
+		var query = connection.query("SELECT * FROM pawn", function(err, rows) {
+			if (err) console.log("Error Selecting : %s", err);
+			res.render("pages/pawn", { title: "All Parts", rows: rows });
 		});
 		connection.release();
 	});
 };
+
+exports.gpioon = function(req, res) {
+	var uuid = req.body.uuid;
+	var exec = require("child_process").exec;
+	exec("gpio -g mode 18 out" , function(error, stdout, stderr) {
+		console.log("stdout: " + stdout);
+		console.log("stderr: " + stderr);
+		if (error !== null) {
+			console.log("exec error: " + error);
+		}
+	});
+};
+
+exports.gpiooff = function(req, res) {
+	var uuid = req.body.uuid;
+	var exec = require("child_process").exec;
+	exec("gpio -g mode 18 in", function(error, stdout, stderr) {
+		console.log("stdout: " + stdout);
+		console.log("stderr: " + stderr);
+		if (error !== null) {
+			console.log("exec error: " + error);
+		}
+	});
+};
+
+exports.gpiotoggle = function(req, res) {
+	var uuid = req.body.uuid;
+	var exec = require("child_process").exec;
+	exec("gpio -g mode 18 out", function(error, stdout, stderr) {
+		console.log("stdout: " + stdout);
+		console.log("stderr: " + stderr);
+		if (error !== null) {
+			console.log("exec error: " + error);
+		}
+	});
+	var exec = require("child_process").exec;
+	exec("gpio -g mode 18 in", function(error, stdout, stderr) {
+		console.log("stdout: " + stdout);
+		console.log("stderr: " + stderr);
+		if (error !== null) {
+			console.log("exec error: " + error);
+		}
+	});
+};
+
+ 
